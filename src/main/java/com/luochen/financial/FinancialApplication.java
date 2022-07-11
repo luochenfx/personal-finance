6071bfbdf51634f2dd34978aeb28a843d074c351
cyj <luochenex@gmail.com>
2022-07-11 11:15:45 +0800

    初始化

---
+++ src/main/java/com/luochen/financial/FinancialApplication.java
@@ -0,0 +1,16 @@
+package com.luochen.financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luochenfx
+ */
+@SpringBootApplication
+public class FinancialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialApplication.class, args);
+	}
+
+}
