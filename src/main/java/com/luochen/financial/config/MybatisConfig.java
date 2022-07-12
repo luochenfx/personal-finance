b8f0fde8c5d1f5cbc2575bf04ab5d7372a202a3d
luochen <luochenex@gmail.com>
2022-07-12 16:09:05 +0800

    mybatis-plus 分页

---
+++ src/main/java/com/luochen/financial/config/MybatisConfig.java
@@ -0,0 +1,25 @@
+package com.luochen.financial.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author luochenfx
+ */
+@Configuration
+public class MybatisConfig {
+
+	/**
+	 * mybatis-plus 分页插件
+	 * @return MybatisPlusInterceptor
+	 */
+	@Bean
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+		return interceptor;
+	}
+}
