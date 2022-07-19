633ad217164e38232754107040837242d51084dd
luochen <luochenex@gmail.com>
2022-07-19 14:24:02 +0800

    trade alipay

---
+++ src/main/java/com/luochen/financial/trade/controller/AlipayTransactionRecordController.java
@@ -0,0 +1,19 @@
+package com.luochen.financial.trade.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author luochen
+ * @since 2022-07-19
+ */
+@RestController
+@RequestMapping("/trade/alipay-transaction-record")
+public class AlipayTransactionRecordController {
+
+}
