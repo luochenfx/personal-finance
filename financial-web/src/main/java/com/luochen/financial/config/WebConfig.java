bc05ddb6c251d0bab549a4b35de18c25c983ee77
luochen <luochenex@gmail.com>
2022-07-20 10:03:47 +0800

    addCorsMappings

--- financial-web/src/main/java/com/luochen/financial/config/WebConfig.java
+++ financial-web/src/main/java/com/luochen/financial/config/WebConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -37,6 +38,26 @@ public class WebConfig implements WebMvcConfigurer {
 		return objectMapper;
 	}
 
+	/**
+	 * 跨域配置
+	 * @param registry 跨域资源共享登记
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		//添加映射路径
+		registry.addMapping("/**")
+				//是否发送Cookie
+				.allowCredentials(true)
+				//放行哪些原始域
+				.allowedOriginPatterns("*")
+				//放行哪些请求方式
+				.allowedMethods("GET", "POST", "PUT", "DELETE")
+				//.allowedMethods("*") //或者放行全部
+				//放行哪些原始请求头部信息
+				.allowedHeaders("*")
+				//暴露哪些原始请求头部信息
+				.exposedHeaders("*");
+	}
 
 
 }
