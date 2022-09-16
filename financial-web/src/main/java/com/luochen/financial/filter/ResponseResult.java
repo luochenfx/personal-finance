53f8d0fff44569a2aa6fb76897b7fbf55f38a05c
luochen <luochenex@gmail.com>
2022-09-16 18:07:01 +0800

    修改描述

--- financial-web/src/main/java/com/luochen/financial/filter/ResponseResult.java
+++ financial-web/src/main/java/com/luochen/financial/filter/ResponseResult.java
@@ -5,8 +5,8 @@ import com.luochen.financial.base.Response;
 import java.lang.annotation.*;
 
 /**
- * <p>标记一个类或方法的返回值是否需要包装</p>
- * <p>返回值包装类{@link Response}</p>
+ * <p>标记一个类或方法的返回值不需要使用{@link Response}包装</p>
+ * @see Response
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
